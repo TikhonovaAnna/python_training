fe9bc434ea88b97c382e53256bf7c762d4346927
Anna <462243@e1.ru>
2019-07-21 23:34:32 +0300

    Выделение фикстуры

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,42 +1,24 @@
 # -*- coding: utf-8 -*-
-#from selenium import webdriver
-
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
 from contact import Contact
 from application2 import Application2
+import pytest
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.app = Application2()
+@pytest.fixture
+def app(request):
+    fixture = Application2()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    
-    def test_add_contact(self):
-        self.app.open_home_page()
-        self.app.login(user="admin", password="secret")
-        self.app.add_contact(Contact(firstname="dsf", dlename="gdfg", lastname="ew", nickname="gdf", title="wer", company="dg",
+
+def test_add_contact(app):
+        app.open_home_page()
+        app.login(user="admin", password="secret")
+        app.add_contact(Contact(firstname="dsf", dlename="gdfg", lastname="ew", nickname="gdf", title="wer", company="dg",
                          address="dg", home="dg", mobile="43", work="sdg", fax="213", email="243", email2="234",
                          email3="245", homepage="fsdf", address2="dsf", phone2="sg", notes="sfghh"))
-        self.app.return_home_page()
-        self.app.logout()
+        app.return_home_page()
+        app.logout()
 
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
     
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-
-
-    def tearDown(self):
+def tearDown(self):
         self.app.destroy()
-
-
-if __name__ == "__main__":
-    unittest.main()
