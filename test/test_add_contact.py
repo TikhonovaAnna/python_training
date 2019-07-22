ef223d27e7f3066aafc98328eb38b514833e7b83
Anna <462243@e1.ru>
2019-07-22 23:33:06 +0300

    Выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,13 +11,13 @@ def app(request):
 
 
 def test_add_contact(app):
-        app.open_home_page()
-        app.login(username="admin", password="secret")
-        app.add_contact(Contact(firstname="dsf", dlename="gdfg", lastname="ew", nickname="gdf", title="wer", company="dg",
+    app.open_home_page()
+    app.session.login(username="admin", password="secret")
+    app.add_contact(Contact(firstname="dsf", dlename="gdfg", lastname="ew", nickname="gdf", title="wer", company="dg",
                          address="dg", home="dg", mobile="43", work="sdg", fax="213", email="243", email2="234",
                          email3="245", homepage="fsdf", address2="dsf", phone2="sg", notes="sfghh"))
-        app.return_home_page()
-        app.logout()
+    app.return_home_page()
+    app.session.logout()
 
     
 def tearDown(self):
