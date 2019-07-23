845a1896a232170be34885d2b3fd2cd652d79b4f
Anna <462243@e1.ru>
2019-07-23 23:55:58 +0300

    созданы тесты для удаления групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,11 +3,7 @@ from model.contact import Contact
 from fixture.application import Application
 import pytest
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
+from model.contact import Contact
 
 
 def test_add_contact(app):
